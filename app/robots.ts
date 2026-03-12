2003a8c95b880c04afa5739bf501efdcbdfc4eef
Coco_mora <kiamchomi@gmail.com>
2026-03-12 02:18:05 -0300

    index.html desindexado XD

--- app/robots.ts
+++ app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: '/api/',
+      disallow: ['/api/', '/migracion_pendiente/'],
     },
     sitemap: 'https://www.siglo21sur.com/sitemap.xml',
   };
